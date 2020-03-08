faf91a054054cae2480dc95c3386a03679897387
Javi <ajgallego@gmail.com>
2020-03-08 13:01:13 +0100

    Add new file to save all constants

---
+++ utilConst.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+X_SUFIX = '_GR'
+Y_SUFIX = '_GT'
+
+WEIGHTS_CNN_FOLDERNAME = 'WEIGHTS_CNN'
+WEIGHTS_DANN_FOLDERNAME = 'WEIGHTS_DANN'
+
+LOGS_CNN_FOLDERNAME = 'LOGS_CNN'
+LOGS_DANN_FOLDERNAME = 'LOGS_DANN'
+
+ARRAY_DBS =['dibco2016','dibco2014','palm0','palm1','phi','ein','sal','voy','bdi','all',
+                        'dibco2016-ic','dibco2014-ic','palm0-ic','palm1-ic','phi-ic','ein-ic','sal-ic',
+                        'voy-ic','bdi-ic','all-ic',
+                        'sal-oe10',
+                        'sal-oe0.4',
+                        'sal-blur30x30']
